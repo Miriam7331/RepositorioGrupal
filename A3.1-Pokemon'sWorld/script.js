476a3c011fba576cccf0928a6f15ccbc9ff25259
AlumG <AlumG@majada.es>
2024-10-30 18:02:10 +0100

    paginas extra añadidas

--- A3.1-Pokemon'sWorld/script.js
+++ A3.1-Pokemon'sWorld/script.js
@@ -3,24 +3,30 @@ document.addEventListener('DOMContentLoaded', () => {
     audio.play();
 });
 
+class head extends HTMLElement {
+    constructor() {
+        super();
+        this.outerHTML = /*html*/ `
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>A3.1 Pokemon's World</title>
+            <link rel="stylesheet" href="../styles.css">
+        </head>
+        `;
+    }
+}
+
 class header extends HTMLElement {
     constructor() {
         super();
         this.outerHTML = /*html*/`
             <header>
                     <div class="logo">
-                    <img src="logo.png" alt="Logo de Pokémon" class="logo">
+                    <img src="../media/logo.png" alt="Logo de Pokémon" class="logo">
                 </div>
                 <div class="titulo">
-                    <h4>
-                        <span>P</span>
-                        <span>O</span>
-                        <span>K</span>
-                        <span>E</span>
-                        <span>M</span>
-                        <span>O</span>
-                        <span>N</span>
-                    </h4>
+                    <h4>POKEMON</h4>
                 </div>
             </header >
         `;
@@ -32,11 +38,11 @@ class aside extends HTMLElement {
         super();
         this.outerHTML = /*html*/ `
         <aside>
-            <img src="logoAdd.png" alt="Logo del Campeonato Mundial de Pokémon" style="width: 100%;" />
+            <img src="../media/logoAdd.png" alt="Logo del Campeonato Mundial de Pokémon" style="width: 100%;" />
             <h2>¡Campeonato Mundial de Pokémon!</h2>
             <p>Únete a los mejores entrenadores de todo el mundo en la competencia más emocionante del año. ¡Demuestra tu habilidad, estrategia y amor por Pokémon en este evento épico!</p>
             <p>Escanea el QR para más información y detalles de inscripción:</p>
-            <img src="qr.png" alt="Código QR" style="width: 100%;" />
+            <img src="../media/qr.png" alt="Código QR" style="width: 100%;" />
         </aside>
     `;
     }
@@ -48,11 +54,11 @@ class nav extends HTMLElement {
         this.outerHTML = /*html*/ `
         <nav>
             <ul>
-                <li>Inicio</li>
-                <li>Bulbasaur</li>
-                <li>Charmander</li>
-                <li>Squirtle</li>
-                <li>Pikachu</li>
+                <li><a href="main.html">Inicio</a></li>
+                <li><a href="bulbasaur.html">Bulbasaur</a></li>
+                <li><a href="charmander.html">Charmander</a></li>
+                <li><a href="squirtle.html">Squirtle</a></li>
+                <li><a href="pikachu.html">Pikachu</a></li>
             </ul>
         </nav>
     `;
@@ -70,6 +76,7 @@ class footer extends HTMLElement {
     }
 }
 
+customElements.define('x-head', head);
 customElements.define('x-header', header);
 customElements.define('x-aside', aside);
 customElements.define('x-nav', nav);
